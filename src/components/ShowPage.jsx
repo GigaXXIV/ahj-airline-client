67821a9b6cccf6e104c44613e3a642a7273f6544
JP <jpcodingjourney@JPs-MacBook-Air.local>
2023-02-09 10:58:53 +1100

    Showpage updates

--- src/components/ShowPage.jsx
+++ src/components/ShowPage.jsx
@@ -9,7 +9,7 @@ const ShowPage = ({ match }) => {
 
   let { flightId } = useParams();
   useEffect(() => {
-    const cols = "ABCD";
+    const cols = "ABCDEFGHI";
     const numRows = 10;
     let newSeats = [];
     for (let rowIndex = 0; rowIndex < numRows; rowIndex++) {
@@ -27,32 +27,45 @@ const ShowPage = ({ match }) => {
     setSelectedSeat(seat);
   };
 
-  const handleBookingConfirmation = async () => {
-    try {
-      const response = await axios.post(`/flights/${match.params.flightId}/book_seat`, {
-        seat: selectedSeat
-      });
-      const updatedBookedSeats = [...bookedSeats, selectedSeat];
-      setBookedSeats(updatedBookedSeats);
-    } catch (error) {
-      console.error(error);
-    }
+
+  const handleBookingConfirmation = () => {
+    alert("Booking Confirmed!");
+    setTimeout(() => {
+      window.location.assign("http://localhost:3001");
+    }, 1);
   };
+  
+  
+  // const handleBookingConfirmation = async () => {
+  //   try {
+  //     const response = await axios.post(`/flights/${match.params.flightId}/book_seat`, {
+  //       seat: selectedSeat
+  //     });
+  //     const updatedBookedSeats = [...bookedSeats, selectedSeat];
+  //     setBookedSeats(updatedBookedSeats);
+  //     alert("Booking Confirmed!");
+  //     window.location.assign("http://localhost:3001");
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  
 
   
   return (
   <div className="container text-center">
   <div className="row align-items-center">
-    <div className="col-md-5">
+  <h1>Available Seats</h1>
+    <div className="col-md-3">
     </div>
-    <div className="col-md-2">
-      <h1>Available Seats</h1>
+    <div className="col-md-6">
+    <div class="row justify-content-evenly">
       <table style={{ height: "300px", overflow: "auto" }}>
         <tbody>
           {seats.map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((seat, seatIndex) => (
-                <td key={seatIndex}>
+                <td key={seatIndex} >
                   {bookedSeats.includes(seat) ? (
                     <span>Booked</span>
                   ) : (
@@ -66,14 +79,30 @@ const ShowPage = ({ match }) => {
           ))}
         </tbody>
       </table>
-      {selectedSeat && (
-        <div>
-          <h2>Selected Seat: {selectedSeat}</h2>
-          <button onClick={handleBookingConfirmation}>Confirm Booking</button>
-        </div>
-      )}
+      </div>
     </div>
-    <div className="col-md-5">
+    {selectedSeat && (
+  <div>
+    <h2>Selected Seat: {selectedSeat}</h2>
+    <form>
+      
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" className="input-field form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" className="input-field form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="credit-card">Credit Card:</label>
+        <input type="text" id="credit-card" className="input-field form-control" />
+      </div>
+      <button onClick={handleBookingConfirmation}>Confirm Booking</button>
+    </form>
+  </div>
+)}
+    <div className="col-md">
     </div>
   </div>
 </div>
