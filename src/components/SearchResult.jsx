056e8ab7c9cef78ba4d87a23820084f24f6f0263
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-02-08 14:42:32 +1100

    dabase connected and displayed

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -5,12 +5,15 @@ class SearchResult extends Component {
         return (
             <div>
                 <h1>Search Result</h1>
-                {console.log("this is from S Result", this.props.searches)}
+                {/* {console.log("this is from S Result", this.props.searches)} */}
                 {this.props.searches.map(
                     search => 
                     <ul>
+                        <li>Date: {search.date}</li>
+                        <li>Flight: {search.flightno} </li>
                         <li> Origin: {search.origin}</li>
                         <li> Destination: {search.destination} </li>
+                        <li>Plane: {search.plane_id} </li>
                     </ul>
 
                 )
