361559c7e3a38e6111e8fded5b6460f8cff4f5a7
JP <jpcodingjourney@JPs-MacBook-Air.local>
2023-02-08 11:01:44 +1100

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-import SearchForFlights from './SearchForFlights';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+import ShowPage from "./ShowPage";
+
+const App = () => {
+  const [planes, setPlanes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/planes.json")
+      .then((response) => {
+        setPlanes(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      {/* Parent Component */}
-      <SearchForFlights /> 
-      
+    <div>
+      {planes.map((plane) => {
+        const { id, name, rows, cols } = plane;
+        return (
+          <div key={id}>
+            {name}: {rows} rows, {cols} columns
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
+
+// "app.js" in React is typically the entry file for a React application, where the React components and the logic for rendering them to the UI are defined. It typically sets up the ReactDOM and renders the root component, which serves as the container for all the other components in the application. The "app.js" file is responsible for bootstrapping the application and setting it up for the browser to run.
